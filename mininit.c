1dcc73239dd19302264077793299d6cef5b9222a
Maarten ter Huurne <maarten@treewalker.org>
2014-10-06 17:10:31 +0200

    Bump version to 2.0.0

--- mininit.c
+++ mininit.c
@@ -109,7 +109,7 @@ int main(int argc, char **argv, char **envp)
 		setlinebuf(kmsg);
 		logfile = kmsg;
 	}
-	INFO("OpenDingux mininit 1.1.0\n");
+	INFO("OpenDingux mininit 2.0.0\n");
 	if (!kmsg) {
 		WARNING("Failed to open '/dev/kmsg': %d\n", errno);
 	}
